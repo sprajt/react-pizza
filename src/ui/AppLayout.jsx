987b36772ff30b60ce840c46451a59d39e1b977e
Marek Ukleja <marek.ukleja@gmail.com>
2024-09-04 20:07:19 +0200

    add fetch order function

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,7 +13,6 @@ function AppLayout() {
       {isLoading && <Loader />}
       <Header />
       <main>
-        <h1>Content</h1>
         <Outlet />
       </main>
 
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -2,9 +2,7 @@ import { useNavigate, useRouteError } from "react-router-dom";
 
 function Error() {
   const navigate = useNavigate();
-
   const error = useRouteError();
-  console.log(error);
 
   return (
     <div>
